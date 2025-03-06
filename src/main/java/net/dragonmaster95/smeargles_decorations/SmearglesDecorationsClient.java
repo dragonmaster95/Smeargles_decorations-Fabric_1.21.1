c13175e64576befd950506adb75a2a4a980cd6e9
dragonmaster95 <ploimark@gmail.com>
2025-03-06 12:13:50 +0100

    initial push

---
+++ src/main/java/net/dragonmaster95/smeargles_decorations/SmearglesDecorationsClient.java
@@ -0,0 +1,10 @@
+package net.dragonmaster95.smeargles_decorations;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SmearglesDecorationsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/dragonmaster95/smeargles_decorations/SmearglesDecorationsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.dragonmaster95.smeargles_decorations;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SmearglesDecorationsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
